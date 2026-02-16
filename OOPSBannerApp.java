97bb6f61ab07dd13810b053d9fb35421cce1b4a9
mh8965-bit <mh8965@srmist.edu.in>
2026-02-16 08:39:00 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Each line represents O   O   P   S
+
+        System.out.println(String.join(" ", " *** ", " **** "));
+        System.out.println(String.join(" ", "*   *", "*     "));
+        System.out.println(String.join(" ", "*   *", "*     "));
+        System.out.println(String.join(" ", "*   *", " **** "));
+        System.out.println(String.join(" ", "*   *", "     *"));
+        System.out.println(String.join(" ", "*   *", "*    *"));
+        System.out.println(String.join(" ", " *** ", " **** "));
+
+    }
+}
